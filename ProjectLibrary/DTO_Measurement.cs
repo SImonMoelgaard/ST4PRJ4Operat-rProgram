68b66a7287fbba663adf780464e85f59e5618f52
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2021-03-22 13:43:19 +0100

    DTO_Measurement og UDPSender tilføjet

---
+++ ProjectLibrary/DTO_Measurement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatoerLibrary
+{
+    public class DTO_Measurement
+    {
+        public DateTime timeStamp { get; set; }
+        public double measurementData { get; set; }
+
+        public DTO_Measurement(DateTime _timestamp, double _measurementData)
+        {
+            timeStamp = _timestamp;
+            measurementData = _measurementData;
+        }
+
+    }
+}
---
+++ ProjectLibrary/UDPSender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace OperatoerLibrary
+{
+    public class UDPSender
+    {
+        private int port = 11000;
+        private IPAddress broadCastIP;
+        private Socket socket;
+        private IPEndPoint endPointIP;
+
+        public void OpenSendPorts()
+        {
+            broadCastIP = IPAddress.Parse("xxx.xxx.xxx"); //Computer vi sender til IVP4 adresse på det givne netværk
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            endPointIP = new IPEndPoint(broadCastIP, port);
+        }
+
+        public void SendMeasurementData(DTO_Measurement measurementData)
+        {
+            
+            var json = JsonConvert.SerializeObject(measurementData);
+            byte[] sendData = Encoding.ASCII.GetBytes(json);
+
+            socket.SendTo(sendData, endPointIP);
+
+        }
+
+
+
+
+
+    }
+}
