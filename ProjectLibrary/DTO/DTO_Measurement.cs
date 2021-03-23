c5f7f75b2a9f30855e790bf9624e76ce7842794e
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2021-03-23 11:44:12 +0100

    hf

--- ProjectLibrary/DTO/DTO_Measurement.cs
+++ ProjectLibrary/DTO/DTO_Measurement.cs
@@ -4,16 +4,14 @@ namespace OperatoerLibrary
 {
     public class DTO_Measurement
     {
-        public DTO_Measurement(DateTime timestamp, double measurementData, double gatingLowerValue,
+        public DTO_Measurement( double measurementData, double gatingLowerValue,
             double gatingUpperValue)
         {
-            TimeStamp = timestamp;
             MeasurementData = measurementData;
             GatingLowerValue = gatingLowerValue;
             GatingUpperValue = gatingUpperValue;
         }
 
-        public DateTime TimeStamp { get; set; }
         public double MeasurementData { get; set; }
 
         public double GatingLowerValue { get; set; }
