4afedc8c359cc73a30653443567dba5e473ab8a2
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2021-03-29 09:56:37 +0200

    Datacontainer added

---
+++ ProjectLibrary/ProducerConsumer/DataContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatoerLibrary.ProducerConsumer
+{
+   public  class DataContainer
+    {
+        public double Sample { get; set; }
+    }
+}
