25cf627741ca7bd2329cb398a8e5df604e4bb276
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2021-04-12 13:48:21 +0200

    BaselineFilter Implemented

--- ProjectLibrary/Filters/BaselineFilter.cs
+++ ProjectLibrary/Filters/BaselineFilter.cs
@@ -1,29 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace OperatoerLibrary.Filters
 {
     public class BaselineFilter
     {
-        
+        private List<double> pointstaken = new List<double>();
+        private List<double> baselineValues = new List<double>();
+        private int goingdown;
+        private double lastnumber = 0;
+        private double baseLineValue;
 
-        public double baseLineFilter(List<DTO_Measurement> measurementList)
+        public void GetBaseLineValue(List<DTO_Measurement> measurementlist)
         {
-            double baseline = 0;
-
-            foreach (var sample in measurementList)
+            foreach (var data in measurementlist)
             {
+                pointstaken.Add(data.MeasurementData);
+                if (data.MeasurementData < lastnumber)
+                {
+                    goingdown++;
+
+                    if (goingdown >=3)
+                    {
+                        baselineValues.Add(pointstaken.Min());
+                        pointstaken.Clear();
+                    }
+                }
+                else
+                {
+                    goingdown = 0;
+                }
+
                 
+                lastnumber = data.MeasurementData;
             }
 
+            CalculateBaseLineValue(baselineValues);
 
 
-
-
-
-            return baseline;
         }
+
+        public void CalculateBaseLineValue(List<double> data)
+        {
+            
+            baseLineValue = data.Sum() / data.Count;
+            
+        }
+
+        public double BaseLineAdjustBreathingValue(double dataPoint)
+        {
+            return dataPoint - baseLineValue;
+        }
+
+
 
 
     }
