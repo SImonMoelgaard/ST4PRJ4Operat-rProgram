f74f8028eccf09e62e9bb2b54d63daaed488fba9
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2021-04-26 13:14:02 +0200

    d

---
+++ ProjectLibrary/DTO/DTO_GatingValues.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatoerLibrary.DTO
+{
+    public class DTO_GatingValues
+    {
+        public double UpperGatingValue { get; set; }
+        public double LowerGatingValue { get; set; }
+
+        public DTO_GatingValues(double upperGatingValue, double lowerGatingValue)
+        {
+            UpperGatingValue = upperGatingValue;
+            LowerGatingValue = lowerGatingValue;
+        }
+
+
+
+    }
+}
